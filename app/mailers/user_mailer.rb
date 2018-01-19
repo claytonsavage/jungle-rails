afe775fdaab72bc4f8c14b8c2b873a5698a9fe28
claytonsavage <clytnsavage@gmail.com>
2018-01-19 01:42:22 +0000

    added the ability to email someone with their purchase information

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,15 @@
 class UserMailer < ApplicationMailer
+	default from: 'notifications@example.com'
+
+	def welcome_email(user)
+	
+    #@user = user.stripeEmail
+    @user = user
+    p @user
+    @products = Product.joins(:line_items).where("order_id =?", @user.id)
+    @line_items = LineItem.where("order_id =?", @user.id)
+    # @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: "Order number #{@user.id}")
+  end
 end
+ 
