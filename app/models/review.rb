6430dcf6fd3dd90f21a41f63b0e311226da3f61f
claytonsavage <clytnsavage@gmail.com>
2018-01-20 02:02:06 +0000

    changed the input field of the review section rating to a drop down list

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,5 @@ class Review < ActiveRecord::Base
 	belongs_to :product
 
   validates :description, presence: true
-  # validates :product_id, presence: true
-  # validates :user_id, presence: true
   validates :rating, presence: true
 end
